86626c011e7dea1e8418c6f8c261486c1df707d1
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-08-07 13:03:45 -0400

    update to cards

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,26 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+const articleData = "https://lambda-times-api.herokuapp.com/articles";
+
+axios
+  .get(articleData)
+  .then((response) => {
+    console.log(response);
+
+    const divMainContainer = document.querySelector(".cards-container");
+    const entries = Object.values(response.data.articles);
+
+    entries.forEach((element) => {
+      element.forEach((article) => {
+        divMainContainer.appendChild(articleMaker(article));
+        console.log("Card Created");
+      });
+    });
+  })
+
+  .catch(function (error) {
+    console.log(error);
+  });
